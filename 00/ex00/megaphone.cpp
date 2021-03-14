bc18fae45539b29684433fbb24aaecb00eb13343
jamfrii <lopezandresgonzalo@gmail.com>
2021-03-14 13:55:55 +0100

    CPP00 ex00 finished

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        int i = 1;
+        while (argv[i] != NULL)
+        {
+            int j = 0;
+            while (argv[i][j])
+            {
+                std::cout << (char)toupper(argv[i][j]);
+                j++;
+            }
+            std::cout << std::endl;
+            i++;
+        }
+
+    }
+}
