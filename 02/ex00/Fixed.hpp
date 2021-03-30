cfb04ccf74e0c0c7c9c574771050723c6c651f38
Gonzalo Lopez andres <glopez-a@c1r9s4.42madrid.com>
2021-03-30 19:02:49 +0200

    02 -> ex00 finish

--- 02/ex00/Fixed.hpp
+++ 02/ex00/Fixed.hpp
@@ -1,14 +1,22 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+# include <iostream>
+# include <istream>
+# include <string>
 
 class Fixed
 {
 private:
-    /* data */
+    int value;
+    static const int    bits = 8;
 public:
     Fixed(/* args */);
+    Fixed(Fixed const &other);
     ~Fixed();
+    Fixed   &operator=(Fixed const &other);
+    int     getRawBits() const;
+    void    setRawBits(int const raw_bits);
 };
 
 #endif
